e2ead306697152c9fdbafc247e3ea7b49bd86f6b
Rene <flughoernchen1415@gmail.com>
2018-06-20 12:10:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mongoose Crud Operations
+
+just a sample project for getting used to mongoDB and mongoose
---
+++ test/update_test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => done());
+  });
+
+  it('model instance set and save', () => {
+    joe.set('name', 'Alex');
+    joe.save()
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+      });
+  });
+});
